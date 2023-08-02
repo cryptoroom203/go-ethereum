ca120683ff149d3da370e29f3d858756db352913
cryptoroom203 <126661967+cryptoroom203@users.noreply.github.com>
2023-08-02 15:36:53 +0700

    Update gethclient.go

--- ethclient/gethclient/gethclient.go
+++ ethclient/gethclient/gethclient.go
@@ -15,6 +15,7 @@
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
 // Package gethclient provides an RPC client for geth-specific APIs.
+gfhfghgf
 package gethclient
 
 import (
